0f7564c9123578c9eabb01e6cbfcb865474d4097
hemangdigitalxplode <hemang@digitalxplode.com>
2026-01-02 14:26:04 +0530

    project-initialized

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header";
+import Footer from "./components/footer";
+
+import Home from "./Pages/home";
+
+const App = () => {
+  return (
+    <div className="">
+
+      {/* Header – sab routes pe */}
+      <BrowserRouter>
+        <Header />
+
+        {/* Routes */}
+        <main className="">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+
+        {/* Footer – sab routes pe */}
+        <Footer />
+
+      </BrowserRouter>
+
+
+    </div>
+  );
+};
+
+export default App;
